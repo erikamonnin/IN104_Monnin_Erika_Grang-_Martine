3c328070f95c3f1d4842378532050d828c729259
grangein104 <48726498+grangein104@users.noreply.github.com>
2019-04-02 17:48:40 +0200

    Update terminal_user_interface.py

--- terminal_user_interface.py
+++ terminal_user_interface.py
@@ -11,6 +11,19 @@ choice=raw_input('What do you want to do?\n A - add card \n E - edit a card \n D
 		backside=raw_input('Back Side?'\n)
 		my_deck.add_card_to_deck(id_card,subject,topside,backside)
 		print("the card as been succesfully added!")
+		
+		
+	if choice == 'D':
+		id_card=raw_input('Id_card'\n)
+		my_deck.delete_card_from_deck(id_card)
+	
+	if choice == 'L':
+		filename=raw_input('Nom du fichier'\n)
+		my_deck.load_the_deck(filename)
+
+	if choice == 'S':
+		filename=raw_input('Nom du fichier'\n)
+		my_deck.save_the_deck(filename)
 
 	if choice == 'Q':
 		loop_flag=False
