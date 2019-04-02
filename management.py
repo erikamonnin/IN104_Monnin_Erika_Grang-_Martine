1948c6ea5882f44b137e0e993c94a9a4a2602f7d
erikamonnin <48725854+erikamonnin@users.noreply.github.com>
2019-04-02 16:41:35 +0200

    Update management.py

--- management.py
+++ management.py
@@ -15,47 +15,46 @@ class Card:
 		print("Now you can choose between : glass bin, compost, recyclable, bouchons, electronics, piles, ampoules, waste")
 
 
-class deck:
-	def __init__(self,number,cards):
+class Deck:
+	def __init__(self,number,cards,subject):
 	"""
 	number = the number of cards in the deck
 	cards = a list containing all the cards added
 	"""
 		self.number = number
 		self.cards = cards
+		self.subject = subject
 		
-
+		
+		
+	def add_card_to_deck(self,id_card,subject,top_side,back_side):
+	"""
+	this program creates a new card (attributes are the inputs) and add it to the current deck
+	"""
+		new_card = Card(id_card, subject, top_side, back_side)
+		self.cards.append(card)
+		self.number += 1
+		return 
 	
+	def delete_card_from_deck(self,obsolete_id_card):
+	"""
+	this program deletes a card (thanks to its id) from the current deck
+	"""
+		for x in self.cards:
+			if x.identifier==obsolete_id_card:
+				self.cards.pop(x.position)
+				self.number -=1
+		return
 	
-	
-def add_card_to_deck(card,deck):
-	deck.cards.append(card)
-	deck.number += 1
-	return 
 
-
-
-def edit_topside(deck, identity, new_topside):
-	for card in deck.cards:
-		if card.identifer==identity:
-			card.topside= new_topside
-	return
-
-
-
-
-def edit_backside(deck, identity, new_backside):
-	for card in deck.cards:
-		if card.identifer==identity:
-			card.backside= new_backside
-	return
-
-
-
-def delete_card_from_deck(obsolete_card, deck):
-	deck.cards.pop(obsolete_card.position)
-	deck.number -=1
-	return
+	def edit_card(self,card_id,edited_attribute, change):
+	"""
+	this program edits one attribute of a card (the card is find in the deck thanks to its id)
+	"""
+		for x in self.cards:
+			if x.identifier==card_id :		
+				x.edited_attribute = change
+		return
 
 
 
