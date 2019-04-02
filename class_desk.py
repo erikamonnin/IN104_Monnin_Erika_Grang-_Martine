7ab8dc93dc3da5ea96551e8fedbf3e73d3adb46d
erikamonnin <48725854+erikamonnin@users.noreply.github.com>
2019-04-02 15:48:59 +0200

    j'ai créé la class desk
    les méthodes sont:
    - nombre de cartes
    - liste contenant les cartes

---
+++ class_desk.py
@@ -0,0 +1,9 @@
+class deck:
+	def __init__(self,number,cards):
+	"""
+	number = the number of cards in the deck
+	cards = a list containing all the cards added
+	"""
+		self.number = number
+		self.cards = cards
+		
