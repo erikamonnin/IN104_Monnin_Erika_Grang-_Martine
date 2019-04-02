0a9cf0ca2b9ac7359830ad9422d57115940de44a
grangein104 <48726498+grangein104@users.noreply.github.com>
2019-04-02 16:47:22 +0200

    Update management.py

--- management.py
+++ management.py
@@ -2,8 +2,8 @@ import pickle
 
 class Card:
 
-	def __init__(self, identifer, subject, topside, backside, position):
-		self.identifer=identifer   #string du type card_dechet
+	def __init__(self, identifier, subject, topside, backside, position):
+		self.identifier=identifier   #string du type card_dechet
 		self.subject=subject
 		self.topside=topside
 		self.backside=backside
@@ -32,7 +32,7 @@ class Deck:
 	this program creates a new card (attributes are the inputs) and add it to the current deck
 	"""
 		new_card = Card(id_card, subject, top_side, back_side)
-		self.cards.append(card)
+		self.cards.append(new_card)
 		self.number += 1
 		return 
 	
@@ -59,8 +59,8 @@ class Deck:
 
 
 def save_the_deck(deck, filename):
-	#filename est une chaine de caractère entre '' !!!
+	#filename est une chaine de caractère entre 'filename' !!!
 	with_open(filename,_'wb')_as_f:
 		pickle.dump(deck,_f)
-		
+	return	
 		
