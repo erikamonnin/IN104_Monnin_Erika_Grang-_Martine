7a0a412f2fb328f0b063a1421c6d294430805937
Grange Martine <martine.grange@ensta-paristech.fr>
2019-04-15 18:15:01 +0200

    premier jet

---
+++ training.py
@@ -0,0 +1,40 @@
+
+##########   REVIEW SYSTEM   ###########
+# Five bins : 
+#	1- reviewed each day
+#	2- reviewed all 2 days
+#	3- reviewed all 5 days
+#	4- reviewed all 7 days
+#	5- reviewed all 20 days
+########################################
+
+
+from cardclass import Card
+from class_desk import Deck
+import datetime
+
+
+##### CHARGEMENT DU PAQUET A REVISER   ######
+
+my_deck=Deck()
+filename=raw_input('On which deck do you want to train ?\n') + '.pickle'
+my_deck.load_the_deck(filename)
+
+
+
+
+
+######  USE OF DATETIME   ######
+# D= datetime.datetime.now() alors D= AAAA-MM-DD HH:MM:SS.SSSSSS
+# D.day= DD     D.hour=HH   etc...
+################################
+
+
+
+today=datetime.datetime.now()
+for card in my_deck.cards:
+	if card.date.day != today.day
+		card.review=True
+
+
+
