ab940a1f946f3e847856b4e580c0a606b872f224
Grange Martine <martine.grange@ensta-paristech.fr>
2019-05-07 16:09:50 +0200

    training peaufine git add . tout beau <3

--- training.py
+++ training.py
@@ -55,16 +55,22 @@ random.shuffle(a_revoir)
 
 
 ########     ET MAINTENANT REVISONS !    ##########
+succes = 0
+echecs = 0
+L= 0
+print("\n Courage, seulement %s cartes a reviser aujourd'hui\n" %(len(a_revoir)))
+
 while len(a_revoir)!=0:
 	# Montrer la premiere carte  
 	card=a_revoir[0]
-	
+	L= L+1
+
 	# Reception de la reponse 
 
-	answer=raw_input("\n \n O- Ordures menageres \n R- Recyclage \n C- Compost \n V- Verre \n B- Bouchon \n M- Cendrier \n E- Electronique \n P- Piles \n T- Relais \n A- Ampoules \n Q- Quitter \n  \n Quelle est la poubelle adaptee a ce dechet : %s ? \n" %(card.topside))
+	answer=raw_input("\n O- Ordures menageres \n R- Recyclage \n C- Compost \n V- Verre \n B- Bouchon \n M- Cendrier \n E- Electronique \n P- Piles \n T- Relais \n A- Ampoules \n Q- Quitter \n  \n Quelle est la poubelle adaptee a ce dechet : %s ? \n \n" %(card.topside))
 	answer=answer.upper()
 
-
+	
 	if answer=='Q':
 		my_deck.save_the_deck(filename)
 		quit()
@@ -73,19 +79,27 @@ while len(a_revoir)!=0:
 	if answer==card.backside.upper():
 		print ("Yeah !! Brilliant ! You learn so fast !\n")
 		card.position +=1
-		time.sleep(1.5)
+		succes = succes +1
+		
 
 	###    IF ANSWER WRONG     ###
 	else:
 		print ("Owowo... That was not what was expected\n")
 		print ("%s was expected\n" %(card.backside))
 		card.position = 0
-		time.sleep(1.5)
+		echecs = echecs +1
+		
 
 	###  finalisation   ###
 	card.date=today
 	card.review=False
 	a_revoir.pop(0)
+	ps = int((float(succes)/L)*100)
+	pe = int((float(echecs)/L)*100)
+	print ("Succes : %s %% \nEchecs : %s %%\n \n" %(ps, pe))
+	print("Deja %s cartes revisees ! \nAllez, plus que %s cartes !\n" %(L,len(a_revoir)))
+	time.sleep(1.5)
+	
 
 print("Well done !! You have just finished today's session !! See you tomorrow !!\n")
 
